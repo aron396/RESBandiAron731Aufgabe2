a918c347f279af093852d8bc94521d56a2824dcb
aron396 <bandi.aron2003@gmail.com>
2025-02-18 11:10:45 +0200

    Repositories von Charakter und Produkt

--- src/Repository/CharakterRepository.java
+++ src/Repository/CharakterRepository.java
@@ -1,4 +1,39 @@
 package Repository;
 
+import Model.Charakter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CharakterRepository {
+    private List<Charakter> charakters = new ArrayList<>();
+
+    public void addCharakter(Charakter charakter) {
+        charakters.add(charakter);
+    }
+
+    public void deleteCharakter(int id) {
+        charakters.removeIf(c -> c.getId() == id);
+    }
+
+    public void updateCharakter(int id, Charakter updatedCharakter) {
+        for (int i = 0; i < charakters.size(); i++) {
+            if (charakters.get(i).getId() == id) {
+                charakters.set(i, updatedCharakter);
+                return;
+            }
+        }
+    }
+
+    public List<Charakter> getAllCharakters() {
+        return charakters;
+    }
+
+    public Charakter getCharakterById(int id) {
+        return charakters.stream()
+                .filter(c -> c.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
 }
--- src/Repository/ProduktRepository.java
+++ src/Repository/ProduktRepository.java
@@ -1,4 +1,39 @@
 package Repository;
 
+import Model.Produkt;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProduktRepository {
+    private List<Produkt> produkts = new ArrayList<>();
+
+    public void addProdukt(Produkt produkt) {
+        produkts.add(produkt);
+    }
+
+    public void deleteProdukt(String name) {
+        produkts.removeIf(p -> p.getName().equalsIgnoreCase(name));
+    }
+
+    public void updateProdukt(String name, Produkt updatedProduct) {
+        for (int i = 0; i < produkts.size(); i++) {
+            if (produkts.get(i).getName().equalsIgnoreCase(name)) {
+                produkts.set(i, updatedProduct);
+                return;
+            }
+        }
+    }
+
+    public List<Produkt> getAllProdukts() {
+        return produkts;
+    }
+
+    public Produkt getProductByName(String name) {
+        return produkts.stream()
+                .filter(p -> p.getName().equalsIgnoreCase(name))
+                .findFirst()
+                .orElse(null);
+    }
+
 }
