8e227686e1bdc395c80df8c6ae266a3fe15d6b7b
aron396 <bandi.aron2003@gmail.com>
2025-02-18 11:06:14 +0200

    Produkt und Charakter klassen

--- src/Model/Charakter.java
+++ src/Model/Charakter.java
@@ -1,4 +1,34 @@
 package Model;
 
+import java.util.List;
+
 public class Charakter {
+
+    private int id;
+    private String name;
+    private String herkunftsdorf;
+    private List<Produkt> gekaufteProdukte;
+
+    public Charakter(int id, String name, String herkunftsdorf, List<Produkt> gekaufteProdukte) {
+        this.id = id;
+        this.name = name;
+        this.herkunftsdorf = herkunftsdorf;
+        this.gekaufteProdukte = gekaufteProdukte;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHerkunftsdorf() {
+        return herkunftsdorf;
+    }
+
+    public List<Produkt> getGekaufteProdukte() {
+        return gekaufteProdukte;
+    }
 }
--- src/Model/Produkt.java
+++ src/Model/Produkt.java
@@ -1,4 +1,26 @@
 package Model;
 
 public class Produkt {
+
+    private String name;
+    private double preis;
+    private String herkunftsregion;
+
+    public Produkt(String name, double preis, String herkunftsregion) {
+        this.name = name;
+        this.preis = preis;
+        this.herkunftsregion = herkunftsregion;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPreis() {
+        return preis;
+    }
+
+    public String getHerkunftsregion() {
+        return herkunftsregion;
+    }
 }
