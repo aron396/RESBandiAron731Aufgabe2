ae107f170aa928ec9a8330375461765afa4f47c6
aron396 <bandi.aron2003@gmail.com>
2025-02-18 11:45:31 +0200

    Aufgabe a) und b) - CRUD fur Charakter und Produkt

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import Controller.CharakterController;
+import Controller.ProduktController;
+import Repository.CharakterRepository;
+import Repository.ProduktRepository;
+import View.ConsoleView;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ProduktRepository produktRepository = new ProduktRepository();
+        ProduktController produktController = new ProduktController(produktRepository);
+        CharakterRepository charakterRepository = new CharakterRepository();
+        CharakterController charakterController = new CharakterController(charakterRepository);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ConsoleView consoleView = new ConsoleView(charakterController, produktController);
+        consoleView.showMenu();
     }
 }
